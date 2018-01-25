08f64056eeccb9f3cd66fe3a021d366a26df30a6
penghaiyang <250086721@qq.com>
2018-01-25 11:29:56 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var http = require('http');
+var homepage = require('./routes/homepage');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', homepage);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -23,5 +29,21 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+/**
+ * Create HTTP server.
+ */
+var server = http.createServer(app);
+server.listen(3000);
+server.on('listening', onListening);
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  console.log('Listening on ', addr);
+}
 
 module.exports = app;
