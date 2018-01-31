c30c9ff813a32dd9008d07168e6931852c71d299
penghaiyang home <250086721@qq.com>
2018-01-31 23:21:22 +0800

    update

--- public/js/editIntro.js
+++ public/js/editIntro.js
@@ -5,3 +5,42 @@ $('#modify').on('click', function(){
 $('#add').on('click', function(){
     document.getElementById('modify').innerHTML = "123";
 });
+
+$('#addbtn').click(function(){
+  document.getElementById('modify').innerHTML = "123";
+
+	var realname = $('#realname').val();
+  	var phonenumber = $('#phonenumber').val();
+  	var name = $('#name').val();
+  	var password = $('#password').val();
+  	var intro_phonenumber = $('#intro_phonenumber').val();
+  	var location = $('#location').val();
+
+  	var obj = {
+  		name:name,
+  		password:password,
+  		realname:realname,
+  		phonenumber:phonenumber,
+  		intro_phonenumber:intro_phonenumber,
+  		location:location
+  	}
+    $.ajax({
+    type: 'POST',
+    url: '/admin/intro',
+    data: obj,
+    success: function(data){
+      if(data.status)
+      {
+        window.location.reload();
+      }
+      else
+      {
+        alert(data.info);
+      }
+    },
+    error: function(){
+      alert(data.info);
+    },
+    dataType: 'json'
+  });
+});
