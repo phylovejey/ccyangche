6b286bbcd560288ba1fcb0d2bdb8db6b76bdc2e8
penghaiyang <250086721@qq.com>
2018-01-31 18:01:19 +0800

    update

---
+++ public/js/editIntro.js
@@ -0,0 +1,7 @@
+$('#modify').on('click', function(){
+    document.getElementById('add').innerHTML = "789";
+});
+
+$('#add').on('click', function(){
+    document.getElementById('modify').innerHTML = "123";
+});
