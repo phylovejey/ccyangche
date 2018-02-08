a2da217ddd32b4d4c640040b1a5115071a06d006
penghaiyang <250086721@qq.com>
2018-02-08 11:14:24 +0800

    update

--- routes/item.js
+++ routes/item.js
@@ -18,16 +18,8 @@ router.get('/', function(req, res, next) {
     return res.render('item',data);
 });
 
-//登录成功
-router.get("/item", function(req, res, nex){
-    if(!req.cookies.user){
-        return res.render('homepage',{});
-    }
-    res.render('editItem', { name:req.cookies.user.name});
-});
-
 //插入数据
-router.post('/item', function(req, res, next){
+router.post('/', function(req, res, next){
     console.log("插入数据！", req.body);
     maixiandb.insertData('item', req.body, function(result){
         if(result == 0)
--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var maixiandb = require('../db/maixiandb');
 
-/* GET home page. */
+/* GET Login page. */
 router.get('/', function(req, res, next) {
     console.log("GET Login Page.");
     res.render('login', { title: '买鲜后台管理系统' });
@@ -50,26 +50,6 @@ router.post('/login', function(req, res, next){
                     }
                 }
             }
-        }
-        return res.send(result);
-    });
-});
-
-//登录成功
-router.get("/item", function(req, res, nex){
-    if(!req.cookies.user){
-        return res.render('homepage',{});
-    }
-    res.render('editItem', { title: '商品编辑' });
-});
-
-//插入数据
-router.post('/item', function(req, res, next){
-    console.log("插入数据！", req.body);
-    maixiandb.insertData('item', req.body, function(result){
-        if(result == 0)
-        {
-            console.log("插入失败");
         }
         return res.send(result);
     });
