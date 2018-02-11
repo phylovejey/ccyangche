cef92960945648a3ff43f3f73859597da724e9f9
penghaiyang <250086721@qq.com>
2018-02-11 16:54:27 +0800

    rename

--- public/js/formcallback.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("#itemform").ajaxForm(function(data){
-		if(data.status == 0){
-			alert("增加商品失败:" + data.error);
-		}else{
-			alert("增加商品成功");
-		}
-	});            
-});
---
+++ public/js/maixian.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	$("#itemform").ajaxForm(function(data){
+		if(data.status == 0){
+			alert("增加商品失败:" + data.error);
+		}else{
+			alert("增加商品成功");
+		}
+	});            
+});
+
+/*function searchitem(){
+	$("#btnsearchitem").innerHTMl = "phy";
+}*/
+/*$("#btnsearchitem").click(function(){
+
+
+	$("#btnsearchitem").innerHTMl = "phy";
+
+	  	/*var obj = {
+		target:"item",
+		key:$("#search").val();
+	}
+	$.ajax({
+		url : "/search",
+		type : "GET",
+		data : obj,
+		success : function(result){
+		},
+		error:function(result){
+		}
+	});*/
+});*/
