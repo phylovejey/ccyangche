ccc469cb0aceb4c8dde99cd154e18e29ba503d95
penghaiyang <250086721@qq.com>
2018-02-11 15:26:07 +0800

    update

--- public/js/formcallback.js
+++ public/js/formcallback.js
@@ -1,6 +1,9 @@
-$(document).ready(function() {  
-       $("#itemform").ajaxForm(function(data){  
-             alert("post success.");  
-             //Alert("post success.");  
-       });            
+$(document).ready(function() {
+	$("#itemform").ajaxForm(function(data){
+		if(data.status == 0){
+			alert("增加商品失败:" + data.error);
+		}else{
+			alert("增加商品成功");
+		}
+	});            
 });
