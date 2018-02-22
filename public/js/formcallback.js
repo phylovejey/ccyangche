de14f25a5e0f3e097f9e21a7378122d0f121de81
penghaiyang <250086721@qq.com>
2018-02-22 18:07:33 +0800

    update

--- public/js/formcallback.js
+++ public/js/formcallback.js
@@ -8,10 +8,9 @@ $(document).ready(function() {
 	});  
 });
 
-function mxsearchitem()
-{
+function search(){
 	var obj = {
-		target:"item",
+		target:$("#selecttarget").val(),
 		key:$("#search").val()
 	}
 	$.ajax({
@@ -19,10 +18,10 @@ function mxsearchitem()
 		type : "POST",
 		data : obj,
 		success : function(result){
-			if(result.status == 0){
-				alert(result.error);
+			if(result.result.status == 0){
+				alert(result.result.error);
 			}else{
-				alert(result.info[0].itemname);
+				showsearchresult(result.target, result.result.info);
 			}
 		},
 		error:function(result){
@@ -30,3 +29,95 @@ function mxsearchitem()
 		}
 	});
 }
+
+function showsearchresult(target, data){
+	if(target == 1){
+		showitemreuslt(data);
+	}else if(target == 2){
+
+	}else if(target == 3){
+
+	}
+}
+
+function showitemreuslt(data){
+	var thead = document.getElementById('tablehead');
+	thead.appendChild(getitemhead());
+
+	var tbody = document.getElementById('tablebody');
+    for(var i = 0;i < data.length; i++){
+    	var trow = getitemrow(data[i]);
+    	tbody.appendChild(trow);  
+    }
+}
+
+function getitemhead(){  
+	var row = document.createElement('tr'); //创建行  
+       
+	var itemname = document.createElement('td'); //创建第一列商品名称  
+	itemname.innerHTML = "商品名称"; //填充数据  
+	row.appendChild(itemname); //加入行  ，下面类似  
+       
+	var itemoriginalprice = document.createElement('td');//创建第二列商品原价  
+	itemoriginalprice.innerHTML = "商品原价";  
+	row.appendChild(itemoriginalprice);  
+       
+	var itemcurrentprice = document.createElement('td');//创建第三列商品现价  
+	itemcurrentprice.innerHTML = "商品现价";  
+	row.appendChild(itemcurrentprice);
+
+	var sales = document.createElement('td');//创建第四列商品销量  
+	sales.innerHTML = "商品销量";  
+	row.appendChild(sales); 
+
+	var realinventory = document.createElement('td');//创建第五列商品实际库存  
+	realinventory.innerHTML = "商品实际库存";  
+	row.appendChild(realinventory); 
+
+	var showinventory = document.createElement('td');//创建第六列商品显示库存  
+	showinventory.innerHTML = "商品显示库存";  
+	row.appendChild(showinventory);
+
+	var classify = document.createElement('td');//创建第七列商品分类
+	classify.innerHTML = "商品分类";  
+	row.appendChild(classify);
+
+    return row; //返回tr数据      
+} 
+
+function getitemrow(h){  
+	var row = document.createElement('tr'); //创建行  
+       
+	var itemname = document.createElement('td'); //创建第一列商品名称  
+	itemname.innerHTML = h.itemname; //填充数据  
+	row.appendChild(itemname); //加入行  ，下面类似  
+       
+	var itemoriginalprice = document.createElement('td');//创建第二列商品原价  
+	itemoriginalprice.innerHTML = h.itemoriginalprice;  
+	row.appendChild(itemoriginalprice);  
+       
+	var itemcurrentprice = document.createElement('td');//创建第三列商品现价  
+	itemcurrentprice.innerHTML = h.itemcurrentprice;  
+	row.appendChild(itemcurrentprice);
+
+	var sales = document.createElement('td');//创建第四列商品销量  
+	sales.innerHTML = h.sales;  
+	row.appendChild(sales); 
+
+	var realinventory = document.createElement('td');//创建第五列商品实际库存  
+	realinventory.innerHTML = h.realinventory;  
+	row.appendChild(realinventory); 
+
+	var showinventory = document.createElement('td');//创建第六列商品显示库存  
+	showinventory.innerHTML = h.showinventory;  
+	row.appendChild(showinventory);
+
+	var classify = document.createElement('td');//创建第七列商品分类
+	classify.innerHTML = h.classify;  
+	row.appendChild(classify);
+
+    return row; //返回tr数据      
+}      
+
+
+
