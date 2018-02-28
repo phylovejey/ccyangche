84724e6596f79f4c453b167856e01e486ef393bf
penghaiyang cloud <250086721@qq.com>
2018-02-28 17:47:52 +0800

    update

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var maixiandb = require('../db/maixiandb');
 var https = require('https');
+var util = require('util');
 
 /* GET Login page. */
 router.get('/', function(req, res, next) {
@@ -12,9 +13,13 @@ router.get('/', function(req, res, next) {
 //"https://api.weixin.qq.com/sns/jscode2session?appid=$%s&secret=$%s&js_code=$%s&grant_type=authorization_code";
 router.post('/wxlogin', function(req, res, next){
     var code = req.body.code;
+    console.log("phy wxlogin ", req.body);
+
     var url = util.format("https://api.weixin.qq.com/sns/jscode2session?appid=$%s&secret=$%s&js_code=$%s&grant_type=authorization_code", "wx1de05d1121007dcf", "852ed1bc62f5abbdc31f47a2c8a38612", code);
+    console.log("phy wxlogin url ", url);
+
     https.get(url, function(ress) {
-        console.log(ress.text);
+        console.log(ress);
     }).on('error', function(e) {
         console.log(e.message);   
     });
