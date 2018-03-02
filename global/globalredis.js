6ec321c4d7b309b2006e335ab4aae72fab510fc6
penghaiyang cloud <250086721@qq.com>
2018-03-02 16:06:11 +0800

    update

--- global/globalredis.js
+++ global/globalredis.js
@@ -5,13 +5,15 @@ var globalredis = {};
 //写入JavaScript(JSON)对象
 globalredis.setdata = function(key, value, callback){
     client.hmset(key, value, function(err) {
-        console.log(err);
+        if(err){
+            console.log(err);
+        }
     })
 }
 
 //读取JavaScript(JSON)对象
 globalredis.getdata = function(key, callback){
-    client.get(key, function(err, object) {
+    client.hgetall(key, function(err, object) {
         if(err){
             console.log(err);
         }else{
