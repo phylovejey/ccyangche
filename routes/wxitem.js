a08b4c3b0c0bc9a80d6033659cefae0e4d789d96
penghaiyang cloud <250086721@qq.com>
2018-03-05 20:57:18 +0800

    update

--- routes/wxitem.js
+++ routes/wxitem.js
@@ -2,46 +2,48 @@ var express = require('express');
 var router = express.Router();
 var url = require('url');
 var globalitem = require('../global/globalitem');
-var globalredis = require('../global/globalredis');
-var schedule = require("node-schedule"); 
+var globalredis = require('../global/globalredis'); 
 
 var wxitem = {};
-var rule = schedule.RecurrenceRule();
-rule.hour = [0,6,12,18];
-rule.minute = 0;
- 
-var j = schedule.scheduleJob(rule, function(){
-　　globalitem.findAllItem({"sales":-1}, function(result){
-        generateitempage();
-    });
-});
 
-function generateitempage(){
+router.generateitempage = function(){
     globalitem.findAllItem({"sales":-1}, function(result){
         if(result.status == 0){
             console.log("phy findAllItem error ", result.error);
         }else{
-            console.log("globalitem.findAllItem ", result.info);
-            wxitem = result.info;   
+            wxitem = result.info;
         }
     });    
 }
 
+router.startgenerateitempage = function(){
+    var interval = setInterval(router.generateitempage, 1000*60*60*6);
+}
+
 /* GET wxitem page. */
 router.get('/', function(req, res, next) {
-    var arg = url.parse(req.url, true).query;
-    console.log("Get wxitem arg ", arg);
-
-    if(arg.start != null && arg.end != null){
-        if(wxitem.length <= end){
-            arg.end = wxitem.length - 1;
-        }
-        res.send({status:1, item:wxitem.slice(arg.start, arg.end), end:arg.end});
-    }else{
-        res.send({status:0, error:"参数错误"});
+    if(wxitem.length == 0){
+        router.generateitempage();
     }
-});
 
-generateitempage();
+    var sessionid = req.header("sessionid")
+    globalredis.getdata(sessionid, function(object){
+        if(object != null){
+            var arg = url.parse(req.url, true).query;
+            console.log("Get wxitem arg ", arg);
+
+            if(arg.start != null && arg.end != null){
+                if(wxitem.length <= end){
+                    arg.end = wxitem.length - 1;
+                }
+                res.send({status:1, item:wxitem.slice(arg.start, arg.end), end:arg.end});
+            }else{
+                res.send({status:0, error:"参数错误"});
+            }     
+        }else{
+            res.send({status:0, error:"用户未登录"});
+        }
+    });
+});
 
 module.exports = router;
