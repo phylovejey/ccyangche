971b98770d67a29ea1f7ac6d7b1881b2b080b167
penghaiyang cloud <250086721@qq.com>
2018-03-05 21:36:28 +0800

    update

--- db/maixiandb.js
+++ db/maixiandb.js
@@ -2,13 +2,14 @@ var MongoClient = require('mongodb').MongoClient;
 var DB_CONN_STR = 'mongodb://localhost:27017/maixiandb';
 var maixiandb = {};
 
-maixiandb.connectdb = function(){
+maixiandb.connectdb = function(callback){
 	MongoClient.connect(DB_CONN_STR, function(err, db) {
         if(err){
             console.log("连接失败！");
         }else{
             console.log("连接成功！"); 
             maixiandb.db = db;
+            callback();
         }
 	});
 }
